5b95015dc49f67c870c35beafe23f6052e8bfe08
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2018-10-06 20:36:35 -0300

    Updates

---
+++ precache-manifest.691ed4305385305d898902c519b2fb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6550fcc1b142fcae2d13f339b7084b31",
+    "url": "/static/media/logo.6550fcc1.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e596ef0a5509f302164",
+    "url": "/static/js/main.6e596ef0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6e596ef0a5509f302164",
+    "url": "/static/css/main.12e6b241.chunk.css"
+  },
+  {
+    "revision": "d008897c6bb0710b744b8d2a9aac9a30",
+    "url": "/index.html"
+  }
+];
