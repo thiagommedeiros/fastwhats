2cd47706d358739c3de4d35a915e00d152e0f2b3
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2018-10-06 20:39:30 -0300

    Updates

---
+++ precache-manifest.cc151b6373d536dc40491a7f776c0df2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6550fcc1b142fcae2d13f339b7084b31",
+    "url": "/fastwhats/static/media/logo.6550fcc1.svg"
+  },
+  {
+    "revision": "3bff4790fac18a60f062",
+    "url": "/fastwhats/static/js/runtime~main.3bff4790.js"
+  },
+  {
+    "revision": "80638b7a4433cec242da",
+    "url": "/fastwhats/static/js/main.80638b7a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/fastwhats/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "80638b7a4433cec242da",
+    "url": "/fastwhats/static/css/main.12e6b241.chunk.css"
+  },
+  {
+    "revision": "544ae48231f52d386974604a759cec73",
+    "url": "/fastwhats/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.691ed4305385305d898902c519b2fb9f.js"
+  "/fastwhats/precache-manifest.cc151b6373d536dc40491a7f776c0df2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fastwhats/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
