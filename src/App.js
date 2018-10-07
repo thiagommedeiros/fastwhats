d61ac15168fe7207816674d121c7516c405734e1
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2018-10-06 22:39:05 -0300

    update favicon

--- src/App.js
+++ src/App.js
@@ -27,26 +27,50 @@ class App extends Component {
   render() {
     const { phoneNumber } = this.state
     return (
-      <React.Fragment>
-        <div className="container">
-          <img src={logo} className="logo" alt="FastWhats" />
-          <span>Inicie uma conversa com você mesmo ou com outra pessoa sem precisar salvar na agenda.</span>
-          <div class="input-wrap">
-            <input className="phoneNumber" type="number" placeholder="11999999999" onChange={this.changePhone.bind(this)} />
-            <span>Digite o DDD e o número (apenas números)</span>
-          </div>
-          <button onClick={this.createCall.bind(this)}>Criar conversa</button>
-          <button>Indicar o FastWhats</button>
-          <a
-            className="hidden"
-            ref={this.linkRef}
-            href={`intent://send?text=oi/+55${phoneNumber}#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end`}
-          ></a>
-          <div className="createdBy">
-            Created by: <a href="http://github.com/thiagommedeiros" target="_blank" rel="noopener noreferrer">Thiago Medeiros</a>
-          </div>
+      <div className="container">
+        <img 
+          src={logo}
+          className="logo"
+          alt="FastWhats"
+        />
+        <h1>FastWhats</h1>
+        <p>
+          Inicie uma conversa com você mesmo ou com outra pessoa sem precisar salvar na agenda.
+        </p>
+        <div className="input-wrap">
+          <input 
+            className="phoneNumber" 
+            type="number"
+            placeholder="11999999999"
+            onChange={this.changePhone.bind(this)}
+          />
+          <span>Digite o DDD e o número (apenas números)</span>
         </div>
-      </React.Fragment>
+        <button onClick={this.createCall.bind(this)}>
+          Criar conversa
+        </button>
+        {/*<button>Indicar o FastWhats</button>*/}
+        <a
+          className="hidden"
+          ref={this.linkRef}
+          href={`
+            intent://send/+55${phoneNumber}#Intent;
+            scheme=smsto;
+            package=com.whatsapp;
+            action=android.intent.action.SENDTO;
+            end
+          `}
+        ></a>
+        <div className="createdBy">
+          <a 
+            href="http://github.com/thiagommedeiros" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            Created by Thiago Medeiros
+          </a>
+        </div>
+      </div>
     );
   }
 }
